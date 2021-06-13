61f932308c5d033c27e32c89e5cf6a7348c94b6b
carlosmarruedo <you@example.com>
2021-06-13 10:29:06 +0200

    ultimos retoques de css por todo el codigo. Flex!!

--- src/app/component/profile-picture/profile-picture.component.ts
+++ src/app/component/profile-picture/profile-picture.component.ts
@@ -6,26 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-picture.component.css']
 })
 export class ProfilePictureComponent implements OnInit {
-  
+  //url de la imagen
   url: any = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  //evento que ocurre en el input invisible
   onSelectFile(event: any) {
     if (event.target.files && event.target.files[0]) {
-      var reader = new FileReader();
+      var reader = new FileReader(); 
 
-      reader.readAsDataURL(event.target.files[0]); // read file as data url
+      reader.readAsDataURL(event.target.files[0]); // leer la imagen como url
 
-      reader.onload = (event) => { // called once readAsDataURL is completed
+      reader.onload = (event) => { // cuando carga se llama esta funcion
         this.url = event.target!.result;
       }
     }
-  }
-  public delete(){
-    this.url = null;
   }
 
 }
